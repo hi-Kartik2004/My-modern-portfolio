18f36b0da6dac682a0687f9ab350d09699956f4e
Kartikeya <kudlu2004@gmail.com>
2023-10-08 00:40:55 +0530

    Better on laptop devices - minor css change

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Topbar from '@/components/Topbar';
 export default function Home() {
   return (
     <main className='flex justify-center mt-7 mb-10'>
-      <div className='layout__wrapper flex justify-between max-w-[1310px] w-full px-4'>
+      <div className='layout__wrapper flex justify-between max-w-[1310px] w-full px-4 gap-4'>
        <Sidebar />
 
         <div className='right max-w-[1000px] w-full'>
